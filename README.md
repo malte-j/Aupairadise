9ffcbd6c252dace7c753c9a6d1bc2810f664c5d7
Malte <skyguardian42@gmail.com>
2019-07-14 10:13:12 +0200

    TODO: Blog page, individual posts

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 <h1 align="center">
   Aupairadise
 </h1>
+dns1.p03.nsone.net
+dns2.p03.nsone.net
+dns3.p03.nsone.net
+ 
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -9,7 +9,7 @@ const currYear = new Date().getFullYear();
 export default ({ children }) => (
   <footer className={ css.footer }>
     <p className={css.copyright}>
-      © {currYear} Luise Fuhrhopp
+      © {currYear} Luise Fuhrhop
     </p>
   </footer>
 )
