3ce70146fbecc27c2ee2a27595facb7102a02993
Malte <skyguardian42@gmail.com>
2019-07-28 15:28:37 +0200

    SEO & Favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,6 +21,16 @@ export default ({ children, nav }) => {
     <div className="layout">
       <Helmet>
         <title>{data.site.siteMetadata.title}</title>
+      
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#121221"/>>
+        <meta name="apple-mobile-web-app-title" content="Aupairadise"/>>
+        <meta name="application-name" content="Aupairadise"/>>
+        <meta name="msapplication-TileColor" content="#ffffff"/>>
+        <meta name="theme-color" content="#ffffff"/>>
         <link href="https://fonts.googleapis.com/css?family=PT+Serif&display=swap" rel="stylesheet"></link>
       </Helmet>
 
